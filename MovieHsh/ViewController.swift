04cb3c29f0cf4ac2b9b88bd22d691c2463ac49c5
sjlee <thetomatoaddict@gmail.com>
2022-11-16 01:34:55 +0900

    step2

--- MovieHsh/ViewController.swift
+++ MovieHsh/ViewController.swift
@@ -7,13 +7,27 @@
 
 import UIKit
 
-class ViewController: UIViewController {
+class ViewController: UIViewController, UITableViewDelegate,UITableViewDataSource {
 
+    
+
+    @IBOutlet weak var table: UITableView!
     override func viewDidLoad() {
         super.viewDidLoad()
+        table.delegate = self
+        table.dataSource = self
         // Do any additional setup after loading the view.
     }
-
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return 5
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        tableView.dequeueReusableCell(withIdentifier: "mycell", for: indexPath)
+    }
+    func numberOfSections(in tableView: UITableView) -> Int {
+        return 3
+    }
 
 }
 
