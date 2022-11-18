acf929ff054138774d70650ab7ae3f1b96f9be45
sjlee <thetomatoaddict@gmail.com>
2022-11-18 17:55:46 +0900

    api 데이터 디코드

--- MovieHsh/ViewController.swift
+++ MovieHsh/ViewController.swift
@@ -7,10 +7,20 @@
 
 import UIKit
 let name = ["aaa","bbb","ccc","ddd","eee"]
+struct MovieData : Codable {
+    let boxOfficeResult : BoxOfficeResult
+}
+struct BoxOfficeResult : Codable {
+    let dailyBoxOfficeList : [DailyBoxOfficeList]
+}
+struct DailyBoxOfficeList : Codable {
+    let movieNm : String
+    let audiCnt : String
+}
 class ViewController: UIViewController, UITableViewDelegate,UITableViewDataSource {
-
     
-
+    var movieData : MovieData?
+    
     @IBOutlet weak var table: UITableView!
     let movieURL = "https://www.kobis.or.kr/kobisopenapi/webservice/rest/boxoffice/searchDailyBoxOfficeList.json?key=c71db7da6c938d7e472cfb4fbca2f9b8&targetDt=20221115"
     override func viewDidLoad() {
@@ -21,23 +31,46 @@ class ViewController: UIViewController, UITableViewDelegate,UITableViewDataSourc
         // Do any additional setup after loading the view.
     }
     func getData(){
+        if let url = URL(string: movieURL) {
+            let session = URLSession(configuration: .default)
+            let task = session.dataTask(with: url) { (data, response, error) in
+                if error != nil {
+                    print(error!)
+                    return
+                }
+                if let JSONdata = data {
+                    //print(JSONdata)
+                    let decoder = JSONDecoder()
+                    do {
+                        let decodedData = try decoder.decode(MovieData.self, from: JSONdata)
+                        //print(decodedData.boxOfficeResult.dailyBoxOfficeList[0].movieNm)
+                        //print(decodedData.boxOfficeResult.dailyBoxOfficeList[0].audiCnt)
+                        self.movieData = decodedData
+                        self.table.reloadData()
+                    } catch {
+                        print(error)
+                    }
+                }
+            }
+            task.resume()
+        }
         
     }
     func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
-        return 5
+        return 10
     }
     
     func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
         let cell = tableView.dequeueReusableCell(withIdentifier: "mycell", for: indexPath) as! myTableViewCell
-        cell.movieName.text = name[indexPath.row]
+        cell.movieName.text = movieData?.boxOfficeResult.dailyBoxOfficeList[indexPath.row].movieNm
         return cell
     }
     func numberOfSections(in tableView: UITableView) -> Int {
-        return 2
+        return 5
     }
     func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
         print(indexPath.description)
     }
-
+    
 }
 
